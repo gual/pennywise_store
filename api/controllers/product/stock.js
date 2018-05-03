d9d198c263c1747f8032473c09f1762ea8a201ce
Gual Escalante <gual23@gmail.com>
2018-05-03 00:57:42 -0600

    Added method to update product stock

---
+++ api/controllers/product/stock.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+
+  friendlyName: 'Stock',
+
+
+  description: 'Stock product.',
+
+
+  inputs: {
+    id: {
+      description: 'The ID of the product to update its stock quantity.',
+      type: 'number',
+      example: 1,
+      required: true
+    },
+
+    quantity: {
+      type: 'number',
+      description: 'The product new stock quantity',
+      example: 6,
+      required: true
+    }
+
+  },
+
+
+  exits: {
+    notFound: {
+      description: 'No product with the specified ID was found in the database.',
+      responseType: 'notFound'
+    }
+
+  },
+
+
+  fn: async function (inputs, exits) {
+    var product = await Product.findOne({ id: inputs.id });
+
+    if (!product) { return exits.notFound(); }
+
+    await Product.update({ id: product.id }).set({
+      stock: inputs.quantity
+    });
+    return exits.success();
+
+  }
+
+
+};
--- config/routes.js
+++ config/routes.js
@@ -53,6 +53,8 @@ module.exports.routes = {
   'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
   'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
 
+  'PUT /product/stock/:id': { action: 'product/stock' },
+
   //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
   //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
   //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
