e52f29fb4a2bc18c0317803de16c6b7777fc2196
Gual Escalante <gual23@gmail.com>
2018-05-04 12:43:02 -0600

    Added domain to sockets allowed origins

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://quiet-forest-61795.herokuapp.com'
+    ],
 
 
     /***************************************************************************
