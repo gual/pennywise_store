2ab0df30f1d9c58a91354fb3c5263d59a829ae84
Gual Escalante <gual23@gmail.com>
2018-05-04 11:54:21 -0600

    Updated running port

--- config/env/production.js
+++ config/env/production.js
@@ -337,7 +337,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  // port: 80,
+  port: 80,
 
 
 
